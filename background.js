1d165770a3e8cd620f75179fff0e3a13e0c32bbb
pheehs <ring0pycode@gmail.com>
2012-04-20 19:22:32 +0900

    content scriptを使うことにした

--- background.js
+++ background.js
@@ -6,32 +6,25 @@ var language = "";
 var program = "";
 var code = "";
 
-function checkForValidUrl(tabId, changeInfo, tab) {
-    if (tab.url.match(/^http\:\/\/www\.nhk\.or\.jp\/gogaku\/(\w+)\/(\w+)\/.*/i)) {
-	chrome.pageAction.show(tabId);
-	language = RegExp.$1;
-	program = RegExp.$2;
-	console.log("言語：" + language + "\n講座：" + program);
-
-    }
-};
 
 function checkStreamingURL(details){
     console.log("checkStreamingURL(" + details.type + " type)");
     if (details.url.match(/^http\:\/\/www\.nhk\.or\.jp\/gogaku\/(\w+)\/(\w+)\/(\w+)\/listdataflv.xml.*/i)){
+	language = RegExp.$1;
+	program = RegExp.$2;
 	code = RegExp.$3;
+	console.log("language:" + language);
+	console.log("program:" + program);
 	console.log("code:" + code);
 	url = "http://pythxsh.geek.jp/nhksd/download/?code%3D" + code + "&language%3D" + language + "&program%3D" + program;
-	chrome.pageAction.setPopup({
-		tabId: details.tabId,
-		popup:"popup.html?url="+url,
-	    })
+	chrome.tabs.sendRequest(details.tabId, {url: url}, function(response){
+		console.log(response.farewell);
+	    });
+	console.log(url);
     }
     return details;
 };
 
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
 chrome.webRequest.onCompleted.addListener(
      checkStreamingURL,
      {
